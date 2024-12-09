41af3735c93af9251d8d26c9876f5faad42bea71
gleonard1233 <gleonard@vassar.edu>
2024-12-09 17:57:59 -0500

    ethologycode

--- ethology-code.c
+++ ethology-code.c
@@ -1,3 +1,4 @@
+// Include Libraries
 #include <kipr/wombat.h>  // KIPR Wombat native library
 #include <stdlib.h>       // General-purpose functions
 #include <stdbool.h>      // Boolean support
@@ -16,12 +17,7 @@
 // Define PIN Addresses
 #define RIGHT_IR_PIN 2
 #define LEFT_IR_PIN 3
-#define RIGHT_PHOTO_PIN 0
-#define LEFT_PHOTO_PIN 1
 
-#define FRONT_BUMP_LEFT_PIN 5
-#define FRONT_BUMP_CENTER_PIN 3
-#define FRONT_BUMP_RIGHT_PIN 4
 #define BACK_BUMP_LEFT_PIN 2
 #define BACK_BUMP_CENTER_PIN 0
 #define BACK_BUMP_RIGHT_PIN 1
@@ -39,8 +35,10 @@ int hierarchy_length;
 int timer_duration = 500;
 unsigned long start_time = 0;
 bool have_pollen = false;
-// global variables to store all current sensor values accessible to all functions and updated by the "read_sensors" function
-int right_photo_value, left_photo_value, right_ir_value, left_ir_value, front_bump_left_value, front_bump_center_value, front_bump_right_value, back_bump_left_value, back_bump_center_value, back_bump_right_value;
+
+// store all current sensor values accessible to all functions and updated by the "read_sensors" function
+int right_ir_value, left_ir_value, back_bump_left_value, back_bump_center_value, back_bump_right_value;
+
 // threshold values
 int avoid_threshold = 1600;	   // the absolute difference between IR readings has to be above this for the avoid action
 
@@ -60,13 +58,16 @@ void dance(); // Function for the dance
 void read_sensors();							 // read all sensor values and save to global variables
 bool is_above_distance_threshold(int threshold); // return true if one and only one IR sensor is above the specified threshold
 bool is_back_bump();							 // return true if one of the back bumpers was hit
-void escape_back();
-void avoid();
+void escape_back(); //initialize escape back function
+void avoid(); //initialize avoid function
 
 int spiral_length = 1; // Length of the forward movement, increases over time
 int spin_count = 0; // Global variable to track the number of spins
 
-// Main Function
+//==================================//
+//===============MAIN===============//
+//==================================//
+
 int main() {    
     enable_servo(LEFT_MOTOR_PIN);
     enable_servo(RIGHT_MOTOR_PIN);
@@ -78,57 +79,55 @@ int main() {
     
     initialize_camera();
 
-    while (true) {
-        if (timer_elapsed()) {
-             read_sensors(); //read all sensors and set global variables of their readouts
-             if(is_back_bump())
-            {
-                escape_back();
-                //continue;
-            }
-            else{
-             if(is_above_distance_threshold(avoid_threshold))
-            {
+while (true) {
+    if (timer_elapsed()) {
+        read_sensors(); // Read all sensors and set global variables of their readouts
+
+        if (is_back_bump()) {
+            escape_back();
+            // continue;
+        } else {
+            if (is_above_distance_threshold(avoid_threshold)) {
                 avoid();
-                //continue;
-            }
-            else
-            {
-             if (systime() - no_pollen_timer > 30000) { // Check if 30 seconds have passed without detecting pollen
-                stop();
-                dance(); // Perform the dance
-                no_pollen_timer = systime(); // Reset timer after dance
-                    } else{
-                      if (is_pollinated()) {
+                // continue;
+            } else {
+                if (systime() - no_pollen_timer > 30000) { // Check if 30 seconds have passed without detecting pollen
+                    stop();
+                    dance(); // Perform the dance
+                    no_pollen_timer = systime(); // Reset timer after dance
+                } else {
+                    if (is_pollinated()) {
                         // Object detected, approach it
                         printf("pollinated!!");
-                        // No object detected, continue spinning search
-                        spin_search();
-                    } 
-                    else if (!have_pollen) {
-                      if (search_snapshot(0)) {
-                        // Object detected, approach it
-                        approach_object(0);
-                    } else {
-                        // No object detected, continue spinning search
-                        spin_search();
-                    }
+                        spin_search(); // No object detected, continue spinning search
+                    } else if (!have_pollen) {
+                        if (search_snapshot(0)) {
+                            // Object detected, approach it
+                            approach_object(0);
+                        } else {
+                            // No object detected, continue spinning search
+                            spin_search();
+                        }
                     } else {
                         if (search_snapshot(1)) {
-                        // Object detected, approach it
-                        approach_drop();
-                     } else {
-                        // No object detected, continue spinning search
-                        spin_search();
+                            // Object detected, approach it
+                            approach_drop();
+                        } else {
+                            // No object detected, continue spinning search
+                            spin_search();
+                        }
                     }
                 }
             }
         }
     }
-    }
-    }
-    return 0;
 }
+return 0;
+}
+
+//=======================================//
+//===============FUNCTIONS===============//
+//=======================================//
 
 // Camera Initialization
 void initialize_camera() {
@@ -146,7 +145,8 @@ bool search_snapshot(int channel) {
     }
     return object_count > 0; // Return true if any object is detected
 }
-// Is pollinated Boolean loop
+
+// Is pollinated Boolean loop : checks if the flower the camera sees has pollen on it
 bool is_pollinated() {
     camera_update();
     msleep(10);
@@ -172,10 +172,7 @@ bool is_pollinated() {
         printf("nearby");
         return true;
     }
-    
     return false;
-    
-    
 }
 
 // Dance: Perform a dance by alternating motor movements
@@ -208,52 +205,32 @@ void spin_search() {
 
 // Approach Object: Drives forward until the object is no longer visible, then closes gripper
 void approach_object(channel) {
-    
     stop(); // Stop once the object is no longer visible
-    
     set_servo_position(GRIPPER_PIN, GRIPPER_OPEN_POSITION); // Open the gripper
-
     msleep(1000); // Wait for the gripper to open
-
     while (search_snapshot(channel)) {
         forward(); // Drive forward while object is visible
         msleep(200);
     }
-  
-    
     msleep(1000);
-
     set_servo_position(GRIPPER_PIN, GRIPPER_CLOSED_POSITION); // Close the gripper
-
     have_pollen = true;
-    
     msleep(1000); // Wait for the gripper to open
-
 }
 
 void approach_drop() {
-    
-    stop(); // Stop once the object is no longer visible
-    
-    while (search_snapshot(1)) {
+        stop(); // Stop once the object is no longer visible
+        while (search_snapshot(1)) {
         forward(); // Drive forward while object is visible
         msleep(200);
     }
-    
-    stop();
-
+        stop();
     msleep(1000); // Wait for the gripper to open
-
     set_servo_position(GRIPPER_PIN, GRIPPER_OPEN_POSITION); // Close the gripper
-
-    have_pollen = false;
-    
+    have_pollen = false;  
     msleep(1000); // Wait for the gripper to open
-
     drive(-1.0, -1.0, 0.2);
-
 }
-
 
 // Stop: Stops the robot
 void stop() {
@@ -269,6 +246,8 @@ void forward() {
     set_servo_position(RIGHT_MOTOR_PIN, 750);
 }
 
+//Avoid function
+
 void avoid()
 {
 	if (left_ir_value > avoid_threshold)
@@ -282,12 +261,15 @@ void avoid()
 	}
 }
 
+//escape back function
+
 void escape_back()
 {
 	drive(0.9, 0.9, 0.25); //drive forward a little
 }
 
 // Drive Function: Controls motor speeds
+
 void drive(float left, float right, float delay_seconds) {
     float left_speed = map(left, -1.0, 1.0, 0, 2047);
     float right_speed = map(right, -1.0, 1.0, 2047, 0);
@@ -300,26 +282,25 @@ void drive(float left, float right, float delay_seconds) {
 }
 
 // Timer Elapsed: Checks if specified duration has passed
+
 bool timer_elapsed() {
     return (systime() > (start_time + timer_duration));
 }
 
+//reads all of the sensors
+
 void read_sensors()
 {
-	right_photo_value = analog_et(RIGHT_PHOTO_PIN);			// read the photo sensor at RIGHT_PHOTO_PIN; *** NOTE: greater value means less light ***
-	left_photo_value = analog_et(LEFT_PHOTO_PIN);			// read the photo sensor at LEFT_PHOTO_PIN; *** NOTE: greater value means less light ***
 	right_ir_value = analog_et(RIGHT_IR_PIN);				// read the IR sensor at RIGHT_IR_PIN
 	left_ir_value = analog_et(LEFT_IR_PIN);					// read the IR sensor at LEFT_IR_PIN
 	// read the bumpers
-	front_bump_left_value = digital(FRONT_BUMP_LEFT_PIN);   // read the bumper at FRONT_BUMP_LEFT_PIN
-	front_bump_center_value = digital(FRONT_BUMP_CENTER_PIN); // read the bumper at FRONT_BUMP_CENTER_PIN
-	front_bump_right_value = digital(FRONT_BUMP_RIGHT_PIN);  // read the bumper at FRONT_BUMP_RIGHT_PIN
 	back_bump_left_value = digital(BACK_BUMP_LEFT_PIN);	// read the bumper at BACK_BUMP_LEFT_PIN
 	back_bump_center_value = digital(BACK_BUMP_CENTER_PIN);  // read the bumper at BACK_BUMP_CENTER_PIN
 	back_bump_right_value = digital(BACK_BUMP_RIGHT_PIN);	// read the bumper at BACK_BUMP_RIGHT_PIN	
 }
 /******************************************************/
 
+// Used for avoid function
 
 bool is_above_distance_threshold(int threshold)
 {
@@ -327,12 +308,15 @@ bool is_above_distance_threshold(int threshold)
 	// returns true if one (exclusive) IR value is above the threshold, otherwise false
 }
 
+// Used for Escape back function
+
 bool is_back_bump()
 {
 	return (back_bump_left_value == 1 || back_bump_center_value == 1 || back_bump_right_value == 1); // return true if one of the back bump values is 1, otherwise false
 }
 
 // Map Function: Maps one range to another
+
 float map(float value, float start_range_low, float start_range_high, float target_range_low, float target_range_high) {
     return target_range_low + ((value - start_range_low) / (start_range_high - start_range_low)) * (target_range_high - target_range_low);
 }
